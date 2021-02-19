01e8762e54a24f180e95d3adf81943b8a6678ce0
michamaro <michamaro52@gmail.com>
2021-02-20 00:32:15 +0900

    [add]BaseViewController

---
+++ BookRecord/BaseViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BaseViewController.swift
+//  BookRecord
+//
+//  Created by Mika Miyakawa on 2021/02/20.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
