094bf4c163128b0309cdff1b548049b5594d5841
michamaro <michamaro52@gmail.com>
2021-03-02 03:19:27 +0900

    [add]書籍追加用のコントローラーの追加

---
+++ BookRecord/BookTabBarController.swift
@@ -0,0 +1,29 @@
+//
+//  BookTabBarController.swift
+//  BookRecord
+//
+//  Created by Mika Miyakawa on 2021/03/02.
+//
+
+import UIKit
+
+class BookTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ BookRecord/ViewController/BookSettingViewController.swift
@@ -0,0 +1,15 @@
+//
+//  BookSettingViewController.swift
+//  BookRecord
+//
+//  Created by Mika Miyakawa on 2021/03/02.
+//
+
+import UIKit
+
+class BookSettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
